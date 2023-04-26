ac68f99bdcc342f802d38284cbead1511259883f
KillaGoose51 <andrew.maxey825@gmail.com>
2023-04-26 14:38:59 -0500

    Initial commit

---
+++ Classes_Maxey_Andrew/Greetings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Maxey_Andrew
+{
+    class Greetings
+    {/// <summary>
+    /// 
+    /// </summary>
+        // Public Void (Name) = Method
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Traveler, Welcome");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, thank you for joining us today! Take this sword its dangerous to go alone.");
+        }
+
+    }
+}
---
+++ Classes_Maxey_Andrew/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Classes_Maxey_Andrew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            Greetings greetings = new Greetings(); //
+            //
+            greetings.Welcome();
+
+            //
+            Console.WriteLine("What is your name traveler?");
+            string userName = Console.ReadLine();
+
+            //
+            greetings.Hello(char.ToUpper(userName[0]) + userName.Substring(1));
+
+            Console.WriteLine($"+1 Copper Sword added to {char.ToUpper(userName[0]) + userName.Substring(1)}'s inventory");
+
+
+
+        }
+    }
+}
